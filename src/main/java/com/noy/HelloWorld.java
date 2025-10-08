71267411a0b7ab7d573c5c45656bd36a23c3ed02
noii. <2803038543@qq.com>
2025-10-08 21:16:06 +0800

    工作区1修改

--- src/main/java/com/noy/HelloWorld.java
+++ src/main/java/com/noy/HelloWorld.java
@@ -3,7 +3,7 @@ package com.noy;
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+        System.out.println("Hello,World，im workspace1");
         newMethod();
     }
 
