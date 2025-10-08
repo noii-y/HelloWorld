04a911243c17e83470dc67a20b7f16f5514bd20c
noii. <2803038543@qq.com>
2025-10-08 21:08:04 +0800

    添加了新方法

--- src/main/java/com/noy/HelloWorld.java
+++ src/main/java/com/noy/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Hello,World");
+        newMethod();
+    }
+
+    public static void newMethod(){
+        System.out.println("This is a new Method");
     }
 }
