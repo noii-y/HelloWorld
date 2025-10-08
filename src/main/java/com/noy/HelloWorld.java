825e9e6da822539cdbe8dd281f6b1b5dae9316c6
noii. <2803038543@qq.com>
2025-10-08 21:27:19 +0800

    添加HelloWorld.java文件

--- src/main/java/com/noy/HelloWorld.java
+++ src/main/java/com/noy/HelloWorld.java
@@ -3,7 +3,7 @@ package com.noy;
 public class HelloWorld
 {
     public static void main(String[] args) {
-        System.out.println("Hello,World，im workspace1");
+        System.out.println("Hello,World，im workspace222");
         newMethod();
     }
 
